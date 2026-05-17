351b1179ec5455fbabd97c40fd5613e43f52b559
SKaavya15 <kaavya.s.22ds13@gmail.com>
2026-05-17 13:40:46 +0530

    Delete internship directory

--- internship/pandas3.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-data = pd.read_csv("dataset.csv")
-data["FamilySize"] = data["SibSp"] + data["Parch"]
-result = data.groupby("FamilySize")["Survived"].mean()
-result = result.sort_values(ascending=False)
-print(result)
